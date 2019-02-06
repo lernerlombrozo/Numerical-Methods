825d02ac44fb2ca414cac4228d7736e3a8876948
David Lerner <lernerlombrozo@gmail.com>
2019-02-06 10:03:30 -0700

    reduced to 2 LHS functions

--- solver.js
+++ solver.js
@@ -1,10 +1,10 @@
 min = -1;
 max = 1;
-error=0.00000001;
+error=0.0000000000001;
 
 result = solveForX(min, max, error,"bisection");
 
-console.log(result)
+console.log("result= ", result)
 
 function solveForX(min, max, error , method){
     var Left = error + 1;
@@ -12,31 +12,63 @@ function solveForX(min, max, error , method){
     var ave = (min + max) / 2;
     if(method === "bisection"){
         while ( Math.abs(Left-Right)>error){
-            Left = LHS(ave)
-            Right = RHS(ave)
+            Left = catalysis_eq_constant_presure_LHS(ave);
+            Right = RHS(ave);
             if (Left>Right){
                 max = ave;
             } else {
                 min = ave;
             }
             var ave = (min + max) / 2;
-            console.log(ave)
         }
     }
     return ave;
 }
 
-function LHS(x){
-    //Math.pow(x/(1-x),2)
-    // return x * x * x - 3 * x * x - 3 * x - 4
-    d=4/3-2/3*x
-    return Math.pow((2/3*x/d),2/3)/Math.pow((1/3*(1-x)/d),1/3)/((1-x)/d)/Math.pow((1-1/2*x)*4,2/3)
-}
-
 function RHS(x) {
     return 1.1405
-    // dHR = 100000;
-    // R = 8.314;
-    // To = 400;
-    // 85* Math.exp(dHR/R*(1/To+100*x+1/To))
+}
+
+
+function catalysis_eq_constant_presure_LHS(x) {
+    // aA + bB <-> cC + dD equimolar with no initial products
+    const a = 1;
+    const b = 1 / 3;
+    const c = 2 / 3;
+    const d = 0;
+    const i = 0;
+    const P = 4; //Bar
+    const R = 0.08314; //Bar * L /(Mol * K)
+    const T = 450; // K
+
+    SumV = d + c - (a + b);
+    Nto = a + b + i;
+    Nte = Nto + ((c + d) - (a + b)) * x;
+    //   console.log("Pe= ", Pe);
+    A = Math.pow((a * (1 - x)) / Nte, a);
+    B = Math.pow((b * (1 - x)) / Nte, b);
+    C = Math.pow((c * x) / Nte, c);
+    D = Math.pow((d * x) / Nte, d);
+    return ((C * D) / (A * B)) * Math.pow(P/(R*T), SumV);
+}
+
+function catalysis_eq_constant_volume_LHS(x) {
+    // aA + bB <-> cC + dD equimolar with no initial products
+    const a = 1;
+    const b = 1 / 3;
+    const c = 2 / 3;
+    const d = 0;
+    const i = 0;//1 / 3;
+    const Po = 4; //Bar
+
+    SumV = d + c - (a + b);
+    Nto = a + b + i;
+    Nte = Nto + ((c + d) - (a + b)) * x;
+    Pe = (Nte / Nto) * Po;
+    //   console.log("Pe= ", Pe);
+    A = Math.pow((a * (1 - x)) / Nte, a);
+    B = Math.pow((b * (1 - x)) / Nte, b);
+    C = Math.pow((c * x) / Nte, c);
+    D = Math.pow((d * x) / Nte, d);
+    return ((C*D) / (A*B)) * Math.pow(Pe, SumV);
 }
