d51eac0aa192f955ac9ea1a92cf877032efa5372
David Lerner <lernerlombrozo@gmail.com>
2019-02-05 17:47:29 -0700

    solver works

--- solver.js
+++ solver.js
@@ -1,26 +1,40 @@
-min = -1000;
-max = 1000;
-error=0.0001;
+min = -1;
+max = 1;
+error=0.00000001;
 
 result = solveForX(min, max, error,"bisection");
 
 console.log(result)
 
 function solveForX(min, max, error , method){
+    var Left = error + 1;
+    var Right = 0;
+    var ave = (min + max) / 2;
     if(method === "bisection"){
-        var ave = (min-max)/2
-        LHS(min)
-        RHS(min)
+        while ( Math.abs(Left-Right)>error){
+            Left = LHS(ave)
+            Right = RHS(ave)
+            if (Left>Right){
+                max = ave;
+            } else {
+                min = ave;
+            }
+            var ave = (min + max) / 2;
+            console.log(ave)
+        }
     }
+    return ave;
 }
 
 function LHS(x){
     //Math.pow(x/(1-x),2)
-    return x*x*x - 3*x*x - 3*x -4
+    // return x * x * x - 3 * x * x - 3 * x - 4
+    d=4/3-2/3*x
+    return Math.pow((2/3*x/d),2/3)/Math.pow((1/3*(1-x)/d),1/3)/((1-x)/d)/Math.pow((1-1/2*x)*4,2/3)
 }
 
 function RHS(x) {
-    return 0
+    return 1.1405
     // dHR = 100000;
     // R = 8.314;
     // To = 400;
